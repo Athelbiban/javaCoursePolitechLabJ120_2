1a46b1fe041d35f5db93adc1ce96fdb84c77b52c
athelbiban <vostrovso@gmail.com>
2023-12-31 18:25:25 +0300

    rel: lab2_1

--- src/main/java/frequencyDictionary/FrequencyDictionary.java
+++ src/main/java/frequencyDictionary/FrequencyDictionary.java
@@ -1,16 +1,36 @@
 package frequencyDictionary;
 
-import jdk.jfr.Frequency;
-
-import java.io.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class FrequencyDictionary {
-    public static String[] Reader(String path) {
+
+    private final static String DIRPATH = "reports";
+    private final static String REPORTBYALPH = "report-by-alph.txt";
+    private final static String REPORTBYALPHREV = "report-by-alph-rev.txt";
+    private final static String REPORTBYFREQ = "report-by-freq.txt";
+
+    public static void main(String[] args) {
+
+        Map<String, Integer> stringMap = FrequencyDictionary.reader();
+        List<Map.Entry<String, Integer>> entryList = FrequencyDictionary.getReportByAlph(stringMap);
+        FrequencyDictionary.getReportByFreq(entryList);
+        FrequencyDictionary.getReportByAlphRev(stringMap);
+
+        System.out.println("Отчёты созданы в директории 'reports'");
+
+    }
+
+    private static Map<String, Integer> reader() {
 
         StringBuilder sb = new StringBuilder();
+        String path = "files/j120-lab2.txt";
         File file = new File(path);
-        String reg = "[\\s\\d,.!?'\":;<>/\\[\\]{}«»—\n()…*0123456789]";
+        String reg = "[\\s\\d,.!?'\":;<>/\\[\\]{}«»—\n()…*]";
+        Map<String, Integer> hashMap = new LinkedHashMap<>();
 
         if (file.exists() && file.canRead()) {
             try (FileReader reader = new FileReader(file)) {
@@ -24,26 +44,32 @@ public class FrequencyDictionary {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
+            String[] arr = sb.toString().toLowerCase().split(reg);
+
+            for (String s : arr) {
+                if (!s.isEmpty() && !s.equals("-")) {
+                    hashMap.merge(s, 1, Integer::sum);
+                }
+            }
         }
 
-        return sb.toString().toLowerCase().split(reg);
+        return hashMap;
     }
 
-    public static void getReportByAlph(String[] stringArr) {
+    private static void writer(List<String> list, String fileName) {
 
-        File dir = new File("reports");
-        if (stringArr != null && dir.mkdirs() && dir.canWrite()) {
-            File newFile = new File(dir, "report-by-alph.txt");
+        File dir = new File(DIRPATH);
+        dir.mkdirs();
+
+        if (list != null && dir.canWrite()) {
+            File newFile = new File(dir, fileName);
 
             try (FileWriter writer = new FileWriter(newFile)) {
                 newFile.createNewFile();
-                Arrays.sort(stringArr);
-//                System.out.println(Arrays.toString(stringArr));
 
-                for (String s : stringArr) {
-                    if (!s.isEmpty() && !s.equals("-")) {
-                        writer.write(s + '\n');
-                    }
+                for (String s : list) {
+                    writer.write(s + '\n');
                 }
                 writer.flush();
 
@@ -53,37 +79,54 @@ public class FrequencyDictionary {
         }
     }
 
-    public static void example() {
-        System.out.println("=============ArrayList=============");
-        List<Integer> arrList = new ArrayList<>();
-        addAll(arrList);
-        System.out.println(arrList);
+    private static List<Map.Entry<String, Integer>> getReportByAlph(Map<String, Integer> map) {
 
-        System.out.println("=============LinkedList=============");
-        List<Integer> linkList = new LinkedList<>();
-        addAll(linkList);
-        System.out.println(linkList);
+        List<String> stringList = new LinkedList<>();
+        List<Map.Entry<String, Integer>> entryList = new ArrayList<>(map.entrySet());
+        entryList.sort(Map.Entry.comparingByKey());
 
-        System.out.println("=============HashMap=============");
-        Map<String, Integer> map = new HashMap<>();
-        map.put("a", 1);
-        map.put("b", 2);
-        map.put("c", 3);
-        System.out.println(map.entrySet());
+        for (Map.Entry<String, Integer> e: entryList) {
+            stringList.add(e.getKey());
+        }
 
-        System.out.println("===========LinkedHashMap=========");
-        Map<String, Integer> hashMap = new LinkedHashMap<>();
-        hashMap.put("z", 9);
-        hashMap.put("y", 8);
-        hashMap.put("x", 7);
-        System.out.println(hashMap);
+        writer(stringList, REPORTBYALPH);
+        return entryList;
     }
 
-    static void addAll(Collection<? super Integer> list) {
-        list.add(1);
-        list.add(5);
-        list.add(3);
-        list.add(17);
-        list.add(6);
+    private static void getReportByAlphRev(Map<String, Integer> map) {
+
+        List<String> reversedStringList = new LinkedList<>();
+        List<String> resultList = new LinkedList<>();
+
+        for (Map.Entry<String, Integer> e : map.entrySet()) {
+            StringBuilder sb = new StringBuilder(e.getKey()).reverse();
+            reversedStringList.add(sb.toString());
+        }
+
+        Collections.sort(reversedStringList);
+
+        // здесь кажется, что можно сделать проще, избавившись от этого цикла, но не пойму как
+        for (String s : reversedStringList) {
+            StringBuilder sb = new StringBuilder(s).reverse();
+            resultList.add(sb.toString());
+        }
+
+        writer(resultList, REPORTBYALPHREV);
+
     }
+
+    private static void getReportByFreq(List<Map.Entry<String, Integer>> entryList) {
+
+        List<String> stringList = new LinkedList<>();
+        Collections.reverse(entryList);
+        entryList.sort(Map.Entry.comparingByValue());
+        Collections.reverse(entryList);
+
+        for (Map.Entry<String, Integer> e: entryList) {
+            stringList.add(e.getKey());
+        }
+
+        writer(stringList, REPORTBYFREQ);
+    }
+
 }
--- src/main/java/frequencyDictionary/Main.java
+++
@@ -1,8 +0,0 @@
-package frequencyDictionary;
-
-public class Main {
-    public static void main(String[] args) {
-        String[] stringArr = FrequencyDictionary.Reader("folder1/j120-lab2.txt");
-        FrequencyDictionary.getReportByAlph(stringArr);
-    }
-}
